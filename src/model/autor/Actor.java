75e2c9a08f0742c9595709a04de10f29ddff77c0
Henrique Pacheco <h247165@dac.unicamp.br>
2022-06-17 11:01:52 -0300

    alterado esquema de observers

--- src/model/autor/Actor.java
+++ src/model/autor/Actor.java
@@ -5,27 +5,23 @@ import java.util.ArrayList;
 import model.nivel.IAction;
 import utilidades.Observer;
 import utilidades.Posicao;
-import utilidades.Subject;
 import view.autorView.IVisualActor;
 
-public abstract class Actor implements IActor, Subject{
+public abstract class Actor implements IActor, Observer{
 	protected IAction iaction;
 	protected IVisualActor ivisualactor;
-	private int x, y;
 	private ArrayList<Observer> observers;
 	
 	private String msg; // isso provavelmente nao vai ser uma String
-	private Posicao posicaoAtual;
-	private Posicao posicaoAnterior;
 	
 	
-	private boolean changed;
-	private final Object MUTEX= new Object();
+	protected Posicao posicaoAtual;
+	protected Posicao posicaoAnterior = null; //talvez isso era pra estar so no personagem??
+	
+	
 	
 	public Actor(int x, int y) {
-		this.x = x;
-		this.y =y;
-		this.observers = new ArrayList<>();
+		posicaoAtual = new Posicao(x, y);
 	}
 	
 
@@ -37,64 +33,6 @@ public abstract class Actor implements IActor, Subject{
 		this.ivisualactor = ivisualactor;
 	}
 
-	public int getY() {
-		return y;
-	}
-
-	public void setY(int y) {
-		this.y = y;
-	}
-
-	public int getX() {
-		return x;
-	}
-
-	public void setX(int x) {
-		this.x = x;
-	}
-	
-	@Override
-	public void registrar(Observer obj) {
-		if(obj == null) throw new NullPointerException("Null Observer");
-		synchronized(MUTEX) {
-			if(!observers.contains(obj)) observers.add(obj);
-		}
-	}
-	
-	@Override
-	public void excluirRegistro(Observer obj) {
-		synchronized(MUTEX) {
-			observers.remove(obj);
-		}
-	}
-	
-	@Override
-	public void notificarObservadores() {
-		ArrayList<Observer> observersLocal = null;
-		synchronized(MUTEX) {
-			if(!changed)
-				return;
-			observersLocal = new ArrayList<>(this.observers);
-			this.changed = false;
-		}
-		for (Observer obj : this.observers) {
-			obj.update();
-		}
-	}
-	
-	@Override
-	public Object getUpdate(Observer obj) {
-		Posicao[] movimentos = new Posicao[2];
-		movimentos[0] = this.posicaoAtual;
-		movimentos[1] = this.posicaoAnterior;
-		return movimentos;
-	}
-	
-	public void notificarMovimento(){
-		System.out.println("Movimento realizado de " + posicaoAnterior + " ate " + posicaoAtual);
-		this.changed=true;
-		notificarObservadores();
-	}
 
 	
 	
--- src/model/autor/Personagem.java
+++ src/model/autor/Personagem.java
@@ -1,9 +1,19 @@
 package model.autor;
 
-public class Personagem extends Actor{
+import java.util.ArrayList;
+
+import utilidades.Observer;
+import utilidades.Posicao;
+import utilidades.Subject;
+
+public class Personagem extends Actor implements Subject{
+	private ArrayList<Observer> observers;
+	private boolean changed;
+	private final Object MUTEX= new Object();
 
 	public Personagem(int x, int y) {
 		super(x, y);
+		this.observers = new ArrayList<>();
 	}
 
 	public void acao() {
@@ -12,5 +22,67 @@ public class Personagem extends Actor{
 	public String toString() {
 		return "P";
 	}
+	
+	
+	@Override
+	public void registrar(Observer obj) {
+		if(obj == null) throw new NullPointerException("Null Observer");
+		synchronized(MUTEX) {
+			if(!observers.contains(obj)) observers.add(obj);
+		}
+	}
+	
+	@Override
+	public void excluirRegistro(Observer obj) {
+		synchronized(MUTEX) {
+			observers.remove(obj);
+		}
+	}
+	
+	@Override
+	public void notificarObservadores() {
+		ArrayList<Observer> observersLocal = null;
+		synchronized(MUTEX) {
+			if(!changed)
+				return;
+			observersLocal = new ArrayList<>(this.observers);
+			this.changed = false;
+		}
+		for (Observer obj : this.observers) {
+			obj.update();
+		}
+	}
+	
+	@Override
+	public Object getUpdate(Observer obj) {
+		Posicao[] movimentos = new Posicao[2];
+		movimentos[0] = this.posicaoAtual;
+		movimentos[1] = this.posicaoAnterior;
+		return movimentos;
+	}
+	
+	public void notificarMovimento(){
+		System.out.println("Movimento realizado de " + posicaoAnterior + " ate " + posicaoAtual);
+		this.changed=true;
+		notificarObservadores();
+	}
+	
+	
+	
+	
+	//nao serao usados para personagem !!!! :
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setSubejct(Subject sub) {
+		// TODO Auto-generated method stub
+		
+	}
+	// ate aqui
 	
 }
